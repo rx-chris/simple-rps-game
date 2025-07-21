05f8044415f6c2d91bcdc5da53256ec5792034fe
Christopher <rxchris94@gmail.com>
2025-07-21 23:51:51 +0800

    initial commit - project setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  target: "node",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.cjs",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
